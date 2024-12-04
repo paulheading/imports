c4079d068b25ced1114e21c62b30c327f105a36a
Paul Heading <paulheading@me.com>
2024-12-04 13:07:49 +0000

    merge folders

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,19 @@
+// @ts-check
 import { defineConfig, passthroughImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
-let images = {
-  domains: ["mosaic.scdn.co", "i.scdn.co", "images-ak.spotifycdn.com"],
-};
-
-let vite = {
-  ssr: {
-    noExternal: ["paully"],
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern",
-      },
-    },
-  },
-};
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), sitemap()],
   scopedStyleStrategy: "class",
-  site: "https://paul.ly",
-  output: "static",
-  images,
+  site: "https://yearly.paul.ly",
   image: {
     service: passthroughImageService(),
   },
-  vite,
+  vite: {
+    ssr: {
+      noExternal: ["paully"],
+    },
+  },
 });
